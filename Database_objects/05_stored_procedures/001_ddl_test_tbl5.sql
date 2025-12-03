d67dee744bf76348f5c5b1f8c68f2a879e4cea2d
vinaygwd <vinaygwd4@gmail.com>
2025-12-03 20:03:49 +0530

    Update 001_ddl_test_tbl5.sql

--- Database_objects/05_stored_procedures/001_ddl_test_tbl5.sql
+++ Database_objects/05_stored_procedures/001_ddl_test_tbl5.sql
@@ -2,3 +2,7 @@
 create or alter table DEV_DB.IPM.TEST_TB5
 (
     COMPANY VARCHAR(100), START_DATE DATE);
+
+create or alter table DEV_DB.IPM.TEST_TB7
+(
+    COMPANY VARCHAR(100), START_DATE DATE);
